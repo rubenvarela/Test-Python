87cd4245f82aa8dfa3ee8ff7b535889bc05a1dcb
Rubén Varela <ruben@rubenvarela.com>
2015-01-02 18:24:57 -0400

    Adding a simple requirements.txt and a run.py.
    This is to test the automatic deployment of servers with some common packages used for ML.

---
+++ run.py
@@ -0,0 +1,9 @@
+print("Importing pymongo, networkx, pickle, random, matplotlib...")
+
+import pymongo
+import networkx
+import pickle
+import random
+import matplotlib
+
+print("If you don't see any errors, it means it worked.")
